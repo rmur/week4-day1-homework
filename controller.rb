50bfea0b48a94a169a44fe45154cdad364765b5a
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-18 21:52:26 +0100

    all essential files are added

--- controller.rb
+++ controller.rb
@@ -3,8 +3,14 @@ require('sinatra/contrib/all')
 require('json')
 require_relative('./models/scissor_game')
 
+get '/' do
+  erb(:home)
+end
+
+
+
 get '/game/:player1/:player2' do
   game1 = Game.new(params[:player1], params[:player2])
-  @result = game1.play
-  erb( :result )
+  @result = game1.play 
+  erb( :result ) #using erb file "result" must be exactly result
 end
